4b8caabb7136e1942ef21a100e0420c9b4cd7767
Leshan <leshan0627@gmail.com>
2022-12-29 14:43:28 -0500

    docs: implement Cors in project.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("https://localhost:44445");
+                      });
+});
 
 // Add services to the container.
 
@@ -17,7 +28,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
 
 app.MapControllers();
