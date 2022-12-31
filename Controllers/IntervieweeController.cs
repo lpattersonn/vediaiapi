541dbfbb7144ea2c85811df39bd57a576d5d6aa9
Leshan <leshan0627@gmail.com>
2022-12-31 15:39:56 -0500

    docs: Update intwerviewee apoi to include /api/interviewee

--- Controllers/IntervieweeController.cs
+++ Controllers/IntervieweeController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace vediaiapi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class IntervieweeController : ControllerBase
 {
     public IntervieweeController()
